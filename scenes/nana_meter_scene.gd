fce74e333206e0feccd31ea9a0c97914a3e25f69
myrrhpansies <irisjh@protonmail.com>
2023-04-21 17:41:33 -0700

    script for nanaSlider

--- scenes/nana_meter_scene.gd
+++ scenes/nana_meter_scene.gd
@@ -3,9 +3,23 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+
+
+
+func _on_nana_slide_value_changed(value):
+		print($nanaSlide.value)
+		var nanaValue = $nanaSlide.value
+		$AnimationPlayer.play("nanaAnina")
+		$AnimationPlayer.seek(nanaValue, true)
+
+
+func _on_nana_slide_drag_ended(value_changed):
+	$AnimationPlayer.pause()
